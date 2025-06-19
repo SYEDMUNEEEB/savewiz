e796f3a53bf8056af340dba5b861a210d7dde764
zani-dev <zani-dev03@gmail.com>
2025-06-19 16:21:02 +0500

    allow host

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true,
-    port: 5173,
-    allowedHosts: [
-      'eeb57d6c-6600-4b75-9689-2f360c9d88cb-00-3ltb9p85zypr4.sisko.replit.dev',
-      'localhost' // Also keep localhost allowed
-    ]
-  }
+    host: true, // ← allows LAN access (0.0.0.0)
+    port: 5173, // ← default port or change if needed
+    allowedHosts: true
+  },
+
+  
 })
