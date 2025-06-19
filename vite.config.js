9137b388486309304608788b29dd25ceee6aaae0
zani-dev <zani-dev03@gmail.com>
2025-06-19 16:23:30 +0500

    new

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     host: true, // ← allows LAN access (0.0.0.0)
     port: 5173, // ← default port or change if needed
-    allowedHosts: true
+    allowedHosts: ['.replit.dev'], 
   },
 
   
